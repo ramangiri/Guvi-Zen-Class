08ad7a456b5d2b327465bf2bff2989083c15be53
ramangiri <60056285+ramangiri@users.noreply.github.com>
2021-04-18 20:45:11 +0530

    Add files via upload

---
+++ task 3/prime numbers anonymous.js
@@ -0,0 +1,21 @@
+var sum =function(a){
+    var  res="";
+    for(i=0;i<a.length;i++)
+    {
+        var count=0;
+        for(j=1;j<=a[i];j++)
+        {
+            if(a[i]%j===0)
+            {
+                count++
+            }
+        }
+        if(count==2)
+        {
+            res+=a[i]+" "
+        }
+    }
+    return res;
+ 
+}
+console.log(sum([1,2,3,4,5,6,7,8,9,11]))
